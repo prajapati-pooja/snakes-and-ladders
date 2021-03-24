acdf045d5cc13d7b46ef89aeaf790be77121f9a4
Pooja Prajapati <poojap@Poojas-MacBook-Pro.local>
2021-03-24 20:25:25 +0530

    Add entity square

---
+++ models/Board.go
@@ -0,0 +1,36 @@
+package models
+
+type Board struct {
+	squares []Square
+}
+
+func NewBoard(maxGridSize int, snake Snake) Board {
+	var position int
+	var squares []Square
+	for position = 1; position <= maxGridSize; position++ {
+		square := initializeSquare(position, snake)
+		squares = append(squares, square)
+	}
+
+	return Board{squares: squares}
+}
+
+func initializeSquare(position int, snake Snake) Square {
+	var square Square
+	if position == snake.head {
+		square = SnakeSquare{Snake: snake}
+	} else {
+		square = RegularSquare{Position: position}
+	}
+	return square
+}
+
+func (b Board) firstSquare() Square {
+	return b.squares[0]
+}
+
+
+func (b Board) findNextSquare(square Square, shifts int) Square {
+	nextPosition := square.Shift(shifts)
+	return b.squares[nextPosition-1]
+}
---
+++ models/Board_test.go
@@ -0,0 +1,60 @@
+package models
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBoard_InitializeSquareShouldReturnRegularSquare(t *testing.T) {
+	actualSquare :=  initializeSquare(2, Snake{head: 14, tail: 8})
+	expectedSquare := RegularSquare{Position: 2}
+	assert.Equal(t, expectedSquare, actualSquare)
+}
+
+func TestBoard_InitializeSquareShouldReturnSnakeSquare(t *testing.T) {
+	actualSquare :=  initializeSquare(14, Snake{head: 14, tail: 8})
+	expectedSquare := SnakeSquare{
+		Snake: Snake{head: 14, tail: 8},
+	}
+	assert.Equal(t, expectedSquare, actualSquare)
+}
+
+func TestBoard_FirstSquareShouldReturnFirstSquare(t *testing.T) {
+	board  := NewBoard(100, Snake{head: 14, tail: 8})
+
+	actualSquare :=  board.firstSquare()
+
+	expectedSquare := RegularSquare{Position: 1}
+	assert.Equal(t, expectedSquare, actualSquare)
+}
+
+func TestBoard_FirstSquareShouldNotReturnOtherSquare(t *testing.T) {
+	board  := NewBoard(100, Snake{head: 14, tail: 8})
+
+	actualSquare :=  board.firstSquare()
+
+	expectedSquare := RegularSquare{Position: 2}
+	assert.NotEqual(t, expectedSquare, actualSquare)
+}
+
+func TestBoard_FindNextSquareShouldReturnRelativeNextSquare(t *testing.T) {
+	board  := NewBoard(100, Snake{head: 14, tail: 8})
+
+	firstSquare := RegularSquare{Position: 2}
+	actualSquare :=  board.findNextSquare(firstSquare, 4)
+
+	expectedSquare := RegularSquare{Position: 6}
+	assert.Equal(t, expectedSquare, actualSquare)
+}
+
+func TestBoard_FindNextSquareShouldReturnTailSquare(t *testing.T) {
+	board  := NewBoard(100, Snake{head: 14, tail: 8})
+
+	firstSquare := RegularSquare{Position: 10}
+	actualSquare :=  board.findNextSquare(firstSquare, 4)
+
+	expectedSquare := SnakeSquare{
+		Snake: Snake{head: 14, tail: 8},
+	}
+	assert.Equal(t, expectedSquare, actualSquare)
+}
